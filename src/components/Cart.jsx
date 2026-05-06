63394936f2ec03a66e2bc7d49bca9b52356e664e
federicosimone <federico.simone15@gmail.com>
2026-05-06 10:58:06 +0200

    other Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,10 @@
 function Cart(props) {
     return (
         <>
+            <h2>Il tuo Carrello:</h2>
             <ul>
                 {props.array.map(e => {
-                    return <li>{e.name}, {e.quantity} pz -  € {e.price.toFixed(2)} cad. - TOT: € {(e.price * e.quantity).toFixed(2)}</li>
+                    return <li>{e.name}, {e.quantity} pz -  € {e.price.toFixed(2)} cad. - <strong>TOTALE: € {(e.price * e.quantity).toFixed(2)}</strong></li>
                 })}
             </ul>
         </>
--- src/components/productsList.jsx
+++ src/components/productsList.jsx
@@ -4,12 +4,13 @@ import style from "../components/productsList.module.css";
 function ProductsList(props) {
     return (
         <>
+
             <ul className={style.listaProdotti}>
                 {products.map((product) => {
 
 
 
-                    return <li>{product.name} prezzo: {product.price} <button onClick={() => props.addToCart(product)}>Aggiungi al carrello</button> </li>
+                    return <li>{product.name} € {(product.price).toFixed(2)} <button onClick={() => props.addToCart(product)}>Aggiungi al carrello</button> </li>
                 })}
             </ul>
         </>
